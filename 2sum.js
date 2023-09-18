e4d55e66d3b5fa56359708fa29bf34f51847efc2
hkiame <henrykiame@gmail.com>
2023-09-18 17:25:05 +0300

    adds solution to two-sum problem from leetcode

---
+++ 2sum.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const needle = target - nums[i];
+
+    if (map.has(needle)) {
+      return [i, map.get(needle)];
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return [];
+}
